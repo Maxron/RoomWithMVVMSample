52c79a48becde0b49ee3dd2bbdb472e9ba8d6c92
Wei <zero.maxima@gmail.com>
2018-05-28 17:21:05 +0800

    [Update] Populate some data to database

--- app/src/main/java/com/maxron/roomwithmvvmsample/WordRoomDatabase.java
+++ app/src/main/java/com/maxron/roomwithmvvmsample/WordRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.maxron.roomwithmvvmsample;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 /**
  * Created by maxron on 2018/5/28.
@@ -22,10 +25,39 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class,"word_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
+    }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void>{
+
+        private final WordDao mDao;
+
+        public PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            mDao.deleteAll();
+            Word word = new Word("Hello");
+            mDao.insert(word);
+            word = new Word("World");
+            mDao.insert(word);
+            return null;
+        }
     }
 }
